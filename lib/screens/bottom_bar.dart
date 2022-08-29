eb14969f9e80a170713d8d1abdbc281625324211
Abraham Peter <47416051+theclassical-dev@users.noreply.github.com>
2022-08-29 12:38:20 +0100

    screens, widget and util folders added

---
+++ lib/screens/bottom_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
